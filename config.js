9a91f1748aa810a6a990c9035088e72236105398
nhoctienone1 <92415626+nhoctienone1@users.noreply.github.com>
2022-03-07 09:31:57 +0700

    upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Dì Nhâm';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009157854005';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng dì Nhâm';
+const contentCard = 'Chúc Dì Nhâm 8/3 tràn ngập niềm vui và những nụ cười, luôn mạnh khỏe, hạnh phúc và thành công trong sự nghiệp';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
